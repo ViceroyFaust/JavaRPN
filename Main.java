6f08825bb816cbb1de9a06c1750c167862d68af0
Viceroy Faust <d.ribchinski@gmail.com>
2019-08-23 12:47:27 -0400

    refactored code for more functions

--- Main.java
+++ Main.java
@@ -8,22 +8,28 @@ public class Main {
 
 	public static void main(String[] args) {
 		try {
+			boolean exit = false;
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			Stack<Double> stack = new Stack<Double>();
-			System.out.println("JavaRPN: Input numbers and operands separated by newline");
-			while (true) {
+			System.out.println("JavaRPN: Input numbers and operands separated by newline or space");
+			while (!exit) {
 				String input = reader.readLine();
-				if (input.equals("e")) {
-					break;
-				}
 				String[] inputs = input.split(" ");
 				for (int i = 0; i < inputs.length; i++) {
+					if (inputs[i].equals("e")) {
+						exit = true;
+						break;
+					}
 					if (isNumber(inputs[i])) {
 						stack.push(Double.parseDouble(inputs[i]));
-					} else if (inputs[i].equals("p")) {
-						System.out.println(stack.peek());
-					} else {
-						operator (stack, inputs[i]);
+						continue;
+					} else if (isFunction(inputs[i])) {
+						function(inputs[i], stack);
+						continue;
+					}
+
+					else {
+						operator(stack, inputs[i]);
 					}
 
 				}
@@ -34,6 +40,26 @@ public class Main {
 			e.printStackTrace();
 		}
 
+	}
+
+	private static void function(String string, Stack<Double> stack) {
+		switch (string) {
+		case "p":
+			System.out.println(stack.peek());
+			break;
+		case "s":
+			double num = stack.pop();
+			stack.push(num * num);
+			break;
+		}
+
+	}
+
+	private static boolean isFunction(String input) {
+		if (input.equals("p") || input.equals("s")) {
+			return true;
+		}
+		return false;
 	}
 
 	private static void operator(Stack<Double> stack, String input) {
@@ -51,15 +77,12 @@ public class Main {
 			break;
 		case "/":
 			stack.push(num1 / num2);
-		case "s":
-			stack.push(num1);
-			stack.push(num2 * num2);
-			break;
 		}
 	}
 
 	private static boolean isNumber(String input) {
-		if (input.equals("+") || input.equals("-") || input.equals("*") || input.equals("/") || input.equals("p") || input.equals("s")){
+		if (input.equals("+") || input.equals("-") || input.equals("*") || input.equals("/") || input.equals("p")
+				|| input.equals("s")) {
 			return false;
 		}
 		return true;
