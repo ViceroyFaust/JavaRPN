a66798eb767c3028c8eac5ddcafab86979a642c4
Viceroy Faust <d.ribchinski@gmail.com>
2019-08-27 13:35:21 -0400

    refactored code into different classes and different practices

--- Main.java
+++ Main.java
@@ -1,130 +1,26 @@
 package owl;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
-import java.text.DecimalFormat;
 import java.util.Stack;
 
 public class Main {
 
 	public static void main(String[] args) {
-		try {
 			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 			Stack<Double> stack = new Stack<Double>();
 			System.out.println("JavaRPN: Input numbers and operands separated by newline or space");
-			DecimalFormat df = new DecimalFormat("#,###.#########");
 			while (true) {
-				String input = reader.readLine();
-				String[] inputs = input.split(" ");
-				for (int i = 0; i < inputs.length; i++) {
-					if (isNumber(inputs[i])) {
-						stack.push(Double.parseDouble(inputs[i]));
-						continue;
-					}
-					if (inputs[i].equals("e") || inputs[i].equals("p") || inputs[i].equals("c")) {
-						commands(inputs[i], stack, df);
-					} else if (inputs[i].equals("sq") || inputs[i].equals("sin") || inputs[i].equals("cos")
-							|| inputs[i].equals("tan") || inputs[i].equals("asin") || inputs[i].equals("acos")
-							|| inputs[i].equals("atan")) {
-						function(inputs[i], stack);
-					} else if (inputs[i].equals("+") || inputs[i].equals("-") || inputs[i].equals("*")
-							|| inputs[i].equals("/") || inputs[i].equals("^")) {
-						operator(stack, inputs[i]);
-					} else {
-						System.out.println("ERROR: Invalid input");
-					}
+				try {
+					String input = reader.readLine().toLowerCase();
+					RPNcli.parse(input, stack);
+				} catch (IOException e) {
+					e.printStackTrace();
 				}
+
 			}
-		} catch (
-
-		Exception e) {
-			e.printStackTrace();
-		}
 	}
 
-	private static void commands(String input, Stack<Double> stack, DecimalFormat df) {
-		switch (input) {
-		case "e":
-			System.exit(0);
-			;
-			break;
-		case "p":
-			if (stack.size() > 0) {
-				System.out.println(df.format(stack.peek()));
-				break;
-			} else {
-				System.out.println("ERROR: All Stacks Empty");
-				break;
-			}
-		case "c":
-			stack.clear();
-			break;
-		}
-
-	}
-
-	private static void function(String string, Stack<Double> stack) {
-		if (stack.size() > 0) {
-			double num = stack.pop();
-			switch (string) {
-			case "sq":
-				stack.push(num * num);
-				break;
-			case "sin":
-				stack.push(Math.sin(Math.toRadians(num)));
-				break;
-			case "cos":
-				stack.push(Math.cos(Math.toRadians(num)));
-				break;
-			case "tan":
-				stack.push(Math.tan(Math.toRadians(num)));
-				break;
-			case "asin":
-				stack.push(Math.asin(Math.toRadians(num)));
-				break;
-			case "acos":
-				stack.push(Math.acos(Math.toRadians(num)));
-				break;
-			case "atan":
-				stack.push(Math.atan(Math.toRadians(num)));
-				break;
-			}
-		}
-	}
-
-	private static void operator(Stack<Double> stack, String input) {
-		if (stack.size() > 1) {
-			double num2 = stack.pop();
-			double num1 = stack.pop();
-			switch (input) {
-			case "+":
-				stack.push(num1 + num2);
-				break;
-			case "-":
-				stack.push(num1 - num2);
-				break;
-			case "*":
-				stack.push(num1 * num2);
-				break;
-			case "/":
-				stack.push(num1 / num2);
-				break;
-			case "^":
-				stack.push(Math.pow(num1, num2));
-				break;
-			}
-		} else {
-			System.out.println("ERROR: Can't operate on an empty stack");
-		}
-	}
-
-	private static boolean isNumber(String input) {
-		try {
-			Double.parseDouble(input);
-			return true;
-		} catch (Exception e) {
-			return false;
-		}
-	}
-
+	
 }
---
+++ RPNcli.java
@@ -0,0 +1,137 @@
+package owl;
+
+import java.text.DecimalFormat;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Stack;
+
+public class RPNcli {
+
+	public static void parse(String input, Stack<Double> stack) {
+		String[] parts = input.split(" ");
+		for (int i = 0; i < parts.length; i++) {
+			Double num = isNumber(parts[i]);
+			if (num != null) {
+				stack.push(num);
+			} else
+				operation(parts[i], stack);
+		}
+	}
+
+	private static void operation(String input, Stack<Double> stack) {
+		Set<String> simples = new HashSet<>(Arrays.asList("+", "-", "*", "/", "^"));
+		Set<String> functions = new HashSet<>(Arrays.asList("sin", "cos", "tan", "asin", "acos", "atan", "sq"));
+		Set<String> commands = new HashSet<>(Arrays.asList("p", "e", "c", "pop", "swap"));
+		if (simples.contains(input)) {
+			simple(input, stack);
+		} else if (functions.contains(input)) {
+			function(input, stack);
+		} else if (commands.contains(input)) {
+			command(input, stack);
+		} else {
+			System.out.println("ERROR: Invalid Command");
+		}
+	}
+
+	private static void command(String input, Stack<Double> stack) {
+		DecimalFormat df = new DecimalFormat("#,###.#########");
+		switch (input) {
+		case "e":
+			System.exit(0);
+			break;
+		case "p":
+			if (stack.size() > 0) {
+				System.out.println(df.format(stack.peek()));
+			} else
+				System.out.println("ERROR: no stacks to peek");
+			break;
+		case "c":
+			stack.clear();
+			break;
+		case "pop":
+			if (stack.size() > 0) {
+				stack.pop();
+			} else
+				System.out.println("ERROR: no stacks to pop");
+			break;
+		case "swap":
+			if (stack.size() > 1) {
+				double num2 = stack.pop();
+				double num1 = stack.pop();
+				stack.push(num2);
+				stack.push(num1);
+			} else
+				System.out.println("ERROR: can't swap empty stack");
+		}
+
+	}
+
+	private static void function(String input, Stack<Double> stack) {
+		if (stack.size() > 0) {
+			double num = stack.pop();
+			double rads = Math.toRadians(num);
+			double ans = 0;
+			switch (input) {
+			case "sq":
+				ans = num * num;
+			case "sin":
+				ans = Math.sin(rads);
+				break;
+			case "cos":
+				ans = Math.cos(rads);
+				break;
+			case "tan":
+				ans = Math.tan(rads);
+				break;
+			case "asin":
+				ans = Math.asin(rads);
+				break;
+			case "acos":
+				ans = Math.acos(rads);
+				break;
+			case "atan":
+				ans = Math.atan(rads);
+				break;
+			}
+			stack.push(ans);
+		} else
+			System.out.println("ERROR: can't operate on empty stack");
+	}
+
+	private static void simple(String input, Stack<Double> stack) {
+		if (stack.size() > 1) {
+			double num2 = stack.pop();
+			double num1 = stack.pop();
+			double ans = 0;
+			switch (input) {
+			case "+":
+				ans = num1 + num2;
+				break;
+			case "-":
+				ans = num1 - num2;
+				break;
+			case "*":
+				ans = num1 * num2;
+				break;
+			case "/":
+				ans = num1 / num2;
+				break;
+			case "^":
+				ans = Math.pow(num1, num2);
+				break;
+			}
+			stack.push(ans);
+		} else
+			System.out.println("ERROR: can't operate on empty stack");
+	}
+
+	private static Double isNumber(String input) {
+		try {
+			double num = Double.parseDouble(input);
+			return num;
+		} catch (NumberFormatException n) {
+			return null;
+		}
+	}
+}
